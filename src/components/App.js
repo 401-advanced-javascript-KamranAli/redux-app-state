d997ab7af936e0d45be83df05f10714975b07964
Kamran Ali <kgali21@gmail.com>
2019-11-11 16:42:05 -0800

    deleted random comment

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import Moods from '../containers/Moods';
 import MoodsContainer from '../containers/Moods';
 
 
 export default function App() {
   return <MoodsContainer />;
 }
-
